d031e2b521fcd22bff3e316a51433916e6f06e20
german <angaritagerman@hotmail.com>
2021-03-23 16:34:46 -0500

    repo base

---
+++ Assets/Scripts/NvigateJugar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NvigateJugar : MonoBehaviour
+{
+    public Button buttonJugar;
+
+	void Start () {
+		Button btn = buttonJugar.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClickButtonJugar);
+	}
+
+	void TaskOnClickButtonJugar(){
+		SceneManager.LoadScene("MenuNiveles");
+	}
+}
